a875660b80dbe71caea0fbc3f65f7de5f70f6eab
Hassan <hassannajeeb01@gmail.com>
2026-06-11 20:18:51 +0500

    changed psql url for railway

--- src/main/java/com/example/springvault/SpringVault.java
+++ src/main/java/com/example/springvault/SpringVault.java
@@ -2,8 +2,10 @@ package com.example.springvault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.springvault.repository")
 public class SpringVault {
 
 	public static void main(String[] args) {
